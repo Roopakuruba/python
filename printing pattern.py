8697c2c1ed98fd37514ec4094469a04a23d7e5ff
Roopakuruba <167081843+Roopakuruba@users.noreply.github.com>
2024-04-24 14:12:32 +0530

    Create printing pattern.py

---
+++ printing pattern.py
@@ -0,0 +1,13 @@
+ #printing patren 
+
+    for i in range(5):
+            for j in range(i+1):
+               print(j+1,end=" ")
+           print()
+
+output:
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
